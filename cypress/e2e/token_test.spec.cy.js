b1c357836d6b99f36e87901c5f84f611242c4ea2
Edson Nunes <nunessbs@gmail.com>
2024-10-02 23:54:45 -0300

    cria cenarios de testes

--- cypress/e2e/token_test.spec.cy.js
+++ cypress/e2e/token_test.spec.cy.js
@@ -1,73 +1,181 @@
-describe('Obter Token de Autenticação da Getnet', () => {
+describe('testes Autenticação na API Getnet', () => {
 
   const clientId = '67823c6d-58de-494f-96d9-86a4c22682cb';
   const clientSecret = 'c2d6a06f-5f31-448b-9079-7e170e8536e4';
   const authString = `${clientId}:${clientSecret}`;
+
+  const clientIdInvalid = '67823c6d-58de-494f-96d9-86a4c22682ce';
+  const clientSecretInvalid = 'c2d6a06f-5f31-448b-9079-7e170e8536ee';
+  const authStringInvalid = `${clientIdInvalid}:${clientSecretInvalid}`;
+
   const URL = 'https://api-homologacao.getnet.com.br/auth/oauth/v2/token';
 
-  
-  const authBase64 = btoa(authString);  
+
+  const authBase64 = btoa(authString);
+  const authBase64Invalid = btoa(authStringInvalid);
+
+
+  describe('Testes de Autenticação na API - Cenários de Sucesso', () => {
 
   it('Deve retornar status 200 autenticação com sucesso', () => {
     cy.request({
       method: 'POST',
       url: URL,
       headers: {
-        'Authorization': `Basic ${authBase64}`,  // Header de autenticação
-        'Content-Type': 'application/x-www-form-urlencoded'  // Tipo de conteúdo correto
+        'Authorization': `Basic ${authBase64}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
       },
       body: {
         'grant_type': 'client_credentials',
         'scope': 'oob'
       },
-      form: true  // Isso informa ao Cypress para enviar os dados como `x-www-form-urlencoded`
+      form: true
     }).then((response) => {
-      expect(response.status).to.eq(200);  
-      expect(response.body).to.have.property('access_token');  // Verifique se o token está presente
-      expect(response.body).to.have.property('token_type', 'Bearer');  // Verifique se o tipo de token é "Bearer"
+      expect(response.status).to.eq(200);
       cy.log('Access Token:', response.body.access_token);
     });
   });
 
-  it('Deve retornar status 401 ao chamar a API com campo Authorization vazio', () => {
+  it('Deve validar os campos recebidos em uma requisicao StatusCode 200', () => {
     cy.request({
       method: 'POST',
       url: URL,
       headers: {
-        'Authorization': ``,  //autorização vazia
+        'Authorization': `Basic ${authBase64}`,
         'Content-Type': 'application/x-www-form-urlencoded'
       },
       body: {
-        'grant_type': 'client_credentials',
-        'scope': 'oob'
+        grant_type: 'client_credentials',
+        scope: 'oob'
       },
-      form: true,
-      failOnStatusCode: false  // Permitir falha no status code
+      form: true
     }).then((response) => {
-      expect(response.status).to.eq(401);  // Espera um status 401 Unauthorized
+
+      const { access_token, token_type, expires_in } = response.body;
+      expect(access_token).to.exist;
+      expect(token_type).to.eq('Bearer');
+      expect(expires_in).to.eq(3600);
     });
   });
+});
 
-  it('Deve retornar status 400 ao chamar a API sem campo Authorization', () => {
-    cy.request({
-      method: 'POST',
-      url: URL,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: {
-        'grant_type': 'client_credentials',
-        'scope': 'oob'
-      },
-      form: true,
-      failOnStatusCode: false  // Permitir falha no status code
-    }).then((response) => {
-      // Validação do Status Code
-      expect(response.status).to.eq(400);  // Espera um status 400 Invalid
+
+  describe('Testes de Autenticação na API - Cenários de Erro', () => {
+
+    it('Deve retornar status 401  Authorization vazio', () => {
+      cy.request({
+        method: 'POST',
+        url: URL,
+        headers: {
+          'Authorization': ``,
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: {
+          'grant_type': 'client_credentials',
+          'scope': 'oob'
+        },
+        form: true,
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.eq(401);
+      });
     });
+
+    it('deve retornar status 401 com Authorization invalido', () => {
+      cy.request({
+        method: 'POST',
+        url: URL,
+        headers: {
+          'Authorization': `Basic ${authBase64Invalid}`,
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: {
+          'grant_type': 'client_credentials',
+          'scope': 'oob'
+        },
+        form: true,
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.eq(401);
+      });
+    });
+  
+
+    it('Deve retornar status 400 ao chamar a API sem campo Authorization', () => {
+      cy.request({
+        method: 'POST',
+        url: URL,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'  // Apenas o Content-Type está presente
+        },
+        body: {
+          grant_type: 'client_credentials',  // Tipo de autenticação esperado
+          scope: 'oob'  // Escopo esperado (fixo)
+        },
+        form: true,
+        failOnStatusCode: false  // Permite que a requisição falhe sem interromper o teste
+      }).then((response) => {
+        expect(response.status).to.eq(400);  // Espera-se um status 400
+      });
+    });
+
+
+  
+    it('Deve retornar status 400 ao chamar a API sem o grant_type', () => {
+      cy.request({
+        method: 'POST',
+        url: URL,
+        headers: {
+          'Authorization': `Basic ${authBase64}`,  // Adiciona o header de autorização
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: {
+          scope: 'oob' 
+        },
+        form: true,
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.eq(400);  // Espera-se um status 400
+      });
+    });
+
+    it('Deve retornar status 400 ao chamar a API sem o scope', () => {
+      cy.request({
+        method: 'POST',
+        url: URL,
+        headers: {
+          'Authorization': `Basic ${authBase64}`,  // Adiciona o header de autorização
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: {
+          grant_type: 'client_credentials'
+          //scope: 'oob'  
+        },
+        form: true,
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.eq(400);  // Espera-se um status 400
+      });
+    });
+
+    it('Deve retornar status 400 ao chamar a API sem o Content-Type', () => {
+      cy.request({
+        method: 'POST',
+        url: URL,
+        headers: {
+          'Authorization': `Basic ${authBase64}`  // Adiciona o header de autorização
+        },
+        body: {
+          grant_type: 'client_credentials',
+          scope: 'oob'  
+        },
+        form: true,
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.eq(400);  // Espera-se um status 400
+      });
+    });
+  
+
   });
-
- 
-
-
 });
